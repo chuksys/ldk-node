fcf5a5d92b88d40390976d658024f1c36144f779
Chuks Agbakuru <chuksagbakuru@gmail.com>
2026-02-06 12:41:04 +0100

    fixup! Fix CI flakiness: Provide VSS auth and isolate runtime contexts

--- src/io/vss_store.rs
+++ src/io/vss_store.rs
@@ -992,9 +992,7 @@ mod tests {
 		let mut vss_seed = [0u8; 32];
 		rng.fill_bytes(&mut vss_seed);
 		tokio::task::spawn_blocking(move || {
-			let mut headers = HashMap::new();
-			headers.insert("Authorization".to_string(), "Bearer test".to_string());
-			let header_provider = Arc::new(FixedHeaders::new(headers));
+			let header_provider = Arc::new(FixedHeaders::new(HashMap::new()));
 			let vss_store =
 				VssStore::new(vss_base_url, rand_store_id, vss_seed, header_provider).unwrap();
 
